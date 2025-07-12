c315b5d27c79b8eca2e0f2d742c4fc6288f3f69b
Anonymous dev <kibuukauthuman0@gmail.com>
2025-07-12 22:35:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,19 @@ const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8000;
 
+// Increase event listeners limit
 import('events').then(events => {
     events.EventEmitter.defaultMaxListeners = 500;
 });
 
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
 app.use('/code', code);
 app.use('/pair', async (req, res) => {
     res.sendFile(path.join(__dirname, 'pair.html'));
@@ -26,11 +35,15 @@ app.use('/', async (req, res) => {
     res.sendFile(path.join(__dirname, 'main.html'));
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
+// Start server
 app.listen(PORT, () => {
-    console.log(`YoutTube: @silvaedits254\n\nGitHub: @SilvaTechB\n\nServer running on http://localhost:${PORT}`);
+    console.log(`YouTube: @silvaedits254\n\nGitHub: @SilvaTechB\n\nServer running on http://localhost:${PORT}`);
 });
 
 export default app;
